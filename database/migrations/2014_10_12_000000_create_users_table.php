f15264f661bffd68488b40a75f709259e9c1c971
Mahmoud Slah <mahmodslah25@gmail.com>
2018-02-27 21:47:58 +0200

    fix user table migration & seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->integer('role_id')->unsigned()->default(1);
+            $table->foreign('role_id')->references('role_id')->on('user_roles')->onDelete('cascade');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('created_by')->default(1);
+            $table->integer('active')->default(1)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
+
     }
 
     /**
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,6 +12,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            'role_id' => 1,
+            'created_by' => 1,
+            'active' => 1,
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'password' => bcrypt('6060660'),
